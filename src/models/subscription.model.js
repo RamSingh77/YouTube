c7939f710b2bc5f49747ab28345a25eb270bf2af
Ram Pratap Singh <rp6071229@gmail.com>
2025-04-12 15:33:25 +0530

    fix the  word the subscription

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-const subsciptionSchema = new Schema({
+const subscriptionSchema = new Schema({
      subscriber:{
         type:Schema.Types.ObjectId,  
         ref: "User"
@@ -10,4 +10,4 @@ const subsciptionSchema = new Schema({
      },
 }, {timestamps: true})
 
-export const Subsciption = mongoose.model("Subsciption", subsciptionSchema);
+export const Subsciption = mongoose.model("Subscription", subscriptionSchema);
