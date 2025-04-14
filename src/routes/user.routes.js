07de8ca0a42182f516ea33ddac863cfde3f7a8e0
Ram Pratap Singh <rp6071229@gmail.com>
2025-04-14 13:38:09 +0530

    add /change-password

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser , refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser , refreshAccessToken, changePassword } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js"
 import { verifyJWT  } from "../middlewares/auth.middlewares.js";
 
@@ -29,4 +29,5 @@ router.route("/login").post(loginUser)
 //secured  routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changePassword)
 export default router
